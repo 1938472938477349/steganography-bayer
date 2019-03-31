18f45db9fb3e84a6a53ac9cd11d44dfd0e53c76b
Wei <jerryjerry4103@gmail.com>
2019-03-31 13:49:27 +0200

    readme

--- Steganography.py
+++ Steganography.py
@@ -4,7 +4,7 @@ import cv2
 class Steganography():
 
     @staticmethod
-    def weightedRGB(img1, img2, img3, w1=1, w2=1, w3=1):
+    def weightedRGB(img1, img2, img3, w1=0.1, w2=0.1, w3=1):
         """
         construct an image from r channel of img1, g of img2 and b of img3 with respective weight
         :param img1: First image
@@ -28,20 +28,53 @@ class Steganography():
                     blank_image[i,j,2] =img3[i,j,2]* w3
         return blank_image
 
-im1 = cv2.imread("images/a.jpg", cv2.IMREAD_UNCHANGED)
+
+    @staticmethod
+    def re_weightedRGB(img):
+        """
+
+        :param img:
+        :return:
+        """
+        h = img.shape[0]
+        w= img.shape[1]
+        #img1 = numpy.zeros((h, w, 1), numpy.uint8)
+        #img2 = numpy.zeros((h, w, 1), numpy.uint8)
+        #img3 = numpy.zeros((h, w, 1), numpy.uint8)
+        img_1 = img[:, :, 0]
+        img_2 = img[:, :, 1]
+        img_3 = img[:, :, 2]
+
+        return img_1,img_2,img_3
+
+
+im1 = cv2.imread("images/results/1.png", cv2.IMREAD_UNCHANGED)
 im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
+k,l,m = Steganography.re_weightedRGB(im1)
 
-im2 = cv2.imread("images/b.jpg", cv2.IMREAD_UNCHANGED)
-im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2RGB)
-
-im3 = cv2.imread("images/c.jpg", cv2.IMREAD_UNCHANGED)
-im3 = cv2.cvtColor(im3, cv2.COLOR_BGR2RGB)
-
-k = Steganography.weightedRGB(im1, im2, im3)
-
-k = cv2.cvtColor(k, cv2.COLOR_RGB2BGR)
-
-cv2.imwrite("images/results/1.png", k)
+cv2.imwrite("images/results/3.png", k)
+cv2.imwrite("images/results/4.png", l)
+cv2.imwrite("images/results/5.png", m)
 cv2.imshow('image', k)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+# im1 = cv2.imread("images/a.jpg", cv2.IMREAD_UNCHANGED)
+# im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2RGB)
+#
+# im2 = cv2.imread("images/b.jpg", cv2.IMREAD_UNCHANGED)
+# im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2RGB)
+#
+# im3 = cv2.imread("images/c.jpg", cv2.IMREAD_UNCHANGED)
+# im3 = cv2.cvtColor(im3, cv2.COLOR_BGR2RGB)
+#
+#
+# k = Steganography.weightedRGB(im1, im2, im3)
+#
+# k = cv2.cvtColor(k, cv2.COLOR_RGB2BGR)
+#
+# cv2.imwrite("images/results/2.png", k)
+# cv2.imshow('image', k)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
